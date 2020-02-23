6fc52e771ca54ede690b8eacf63a3e4f3ba59ae2
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-02-23 11:56:43 +0100

    Snack 3

--- snack 3/index.php
+++ snack 3/index.php
@@ -1,1 +1,21 @@
 <!-- Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi <p>. Ogni punto un nuovo paragrafo. -->
+
+<?php
+$text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+$par = explode(".", $text);
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>php-get-params</title>
+  </head>
+  <body>
+    <?php
+      for ($i = 0; $i < count($par); $i++) {
+        echo "<p>" . $par[$i] . "</p>";
+      }
+    ?>
+  </body>
+</html>
